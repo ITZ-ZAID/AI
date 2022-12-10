c12011e4c539e6a864f65bb1ceab9b78f0912131
Zaid <itzzaid@pm.me>
2022-12-10 19:42:18 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,7 @@
 from os import getenv
 
 BOT_TOKEN = getenv("BOT_TOKEN", "")
+MONGO_URL = getenv("MONGO_URL", "")
 BOT_ID = getenv("BOT_ID", "")#token first 10 digits
 AI_API_KEY = getenv("AI_API_KEY", "")
 AI_ID = int(getenv("AI_ID", ""))
