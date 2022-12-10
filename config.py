6b523472dd6e21457f27536ee5eada82f130f9ac
Zaid <itsunknown122@gmail.com>
2022-12-10 19:33:13 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 from os import getenv
 
-BOT_TOKEN = getenv("BOT_TOKEN", "5615542495:AAEusmyIUk7P7T3uJWH-jxyoBAM8d-GW4ik")
-BOT_ID = getenv("BOT_ID", "5615542495")#token first 10 digits
-AI_API_KEY = getenv("AI_API_KEY", "FMxzPoLAnvaRrnn5")
-AI_ID = int(getenv("AI_ID", "170997"))
+BOT_TOKEN = getenv("BOT_TOKEN", "")
+BOT_ID = getenv("BOT_ID", "")#token first 10 digits
+AI_API_KEY = getenv("AI_API_KEY", "")
+AI_ID = int(getenv("AI_ID", ""))
